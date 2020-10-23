dab0537750fbf4d516f5df7ba1051ac8f660d36e
Tim <t232006@yandex.ru>
2020-10-23 21:40:19 +0500

    hometask 2

---
+++ hometask2/hometask2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hometask2
+{
+    class Program
+    {
+        //Приходько. 2) Написать метод подсчета количества цифр числа.
+        static int DigNumber(int num)
+        {
+            if (num / 10 == 0) return 1; else return (1 + DigNumber(num / 10));
+        }
+        static void IODigNumber()
+        {
+            Console.WriteLine("Введите число");
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine(DigNumber(d));
+        }
+        //Приходько. 3) С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+        static int Sum()
+        {
+            int d = int.Parse(Console.ReadLine());
+            if (d == 0) return d; else return (d + Sum());
+        }
+
+
+        static void Main(string[] args)
+        {
+           // IODigNumber();
+            Console.WriteLine(Sum());
+            Console.WriteLine();
+
+        }
+    }
+}
